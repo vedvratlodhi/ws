703893401cc0832f76cd85532c3c3ec9008fd4a7
vlodhi <vlodhi@yuytyu.com>
2018-02-23 11:22:04 +0530

    testing

--- json parse/src/test/JsonEncodeDemo.java
+++ json parse/src/test/JsonEncodeDemo.java
@@ -13,12 +13,7 @@ import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-/*import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-*/
 
 import Bean.Article;
 import Bean.Source;
